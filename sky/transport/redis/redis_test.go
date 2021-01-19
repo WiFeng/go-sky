d7ed7f4fa9e4531c1c29976f2f752e98cf3f86a8
Kevon Liu <admin@521-wf.com>
2021-01-19 17:28:38 +0800

    Add redis test

--- sky/transport/redis/redis_test.go
+++ sky/transport/redis/redis_test.go
@@ -1,2 +1,70 @@
 package redis
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/WiFeng/go-sky/sky/config"
+	"github.com/WiFeng/go-sky/sky/log"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestMain(m *testing.M) {
+	redisConf := []config.Redis{
+		{
+			Name: "redis",
+			Host: "127.0.0.1",
+			Port: 6379,
+			Auth: "",
+			DB:   0,
+		},
+	}
+
+	logConf := config.Log{
+		Level:    zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		Encoding: "json",
+	}
+	if _, err := log.Init(logConf); err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	Init(context.Background(), redisConf)
+
+	os.Exit(m.Run())
+}
+
+func TestSet(t *testing.T) {
+	var ctx = context.Background()
+	redisCli, err := GetInstance(ctx, "redis")
+	if err != nil {
+		t.Error(err)
+	}
+	got, err := redisCli.Set(ctx, "__gotest__:set:key1", "val1", 30*time.Minute).Result()
+	if err != nil {
+		t.Error(err)
+	}
+	if got != "OK" {
+		t.Errorf("redis.Set = %s; want OK", got)
+	}
+}
+
+func TestGet(t *testing.T) {
+	var ctx = context.Background()
+	redisCli, err := GetInstance(ctx, "redis")
+	if err != nil {
+		t.Error(err)
+	}
+
+	redisCli.Set(ctx, "__gotest__:get:key2", "val2", 30*time.Minute)
+	got, err := redisCli.Get(ctx, "__gotest__:get:key2").Result()
+	if err != nil {
+		t.Error(err)
+	}
+	if got != "val2" {
+		t.Errorf("redis.Get = %s; want val2", got)
+	}
+}

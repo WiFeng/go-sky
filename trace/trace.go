ff2ee38fe8f3aad53ec336b127d7c1d6e02ced5a
Kevon Liu <admin@521-wf.com>
2021-02-25 15:46:37 +0800

    fix code

--- trace/trace.go
+++ trace/trace.go
@@ -8,9 +8,7 @@ import (
 
 	"github.com/WiFeng/go-sky/config"
 	"github.com/WiFeng/go-sky/helper"
-	"github.com/uber/jaeger-client-go"
 	jaegerconfig "github.com/uber/jaeger-client-go/config"
-	jaegerlog "github.com/uber/jaeger-client-go/log"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-lib/metrics/prometheus"
@@ -19,10 +17,6 @@ import (
 // Init ...
 func Init(ctx context.Context, serviceName string, cfg config.Trace) {
 	metricsFactory := prometheus.New()
-
-	logger := jaeger.StdLogger
-	// logger := log.GetDefaultLogger()
-	loggerOption := jaegerconfig.Logger(jaegerlog.DebugLogAdapter(logger))
 	tracer, tracerCloser, err := jaegerconfig.Configuration{
 		ServiceName: serviceName,
 		Sampler: &jaegerconfig.SamplerConfig{
@@ -30,16 +24,12 @@ func Init(ctx context.Context, serviceName string, cfg config.Trace) {
 			Param: 1,
 		},
 		Reporter: &jaegerconfig.ReporterConfig{
-			// LogSpans:            true,
-			// LocalAgentHostPort:  "localhost:6831",
-			// BufferFlushInterval: time.Second * 1,
 			CollectorEndpoint:   cfg.Reporter.CollectorEndpoint,
 			LocalAgentHostPort:  cfg.Reporter.LocalAgentHostPort,
 			BufferFlushInterval: cfg.Reporter.BufferFlushInterval * time.Second,
 		},
 	}.NewTracer(
 		jaegerconfig.Metrics(metricsFactory),
-		loggerOption,
 	)
 
 	if err != nil {
